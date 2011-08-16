76ac6fb6ee668b3fcd6c7f83218b6cd1ca9ebf15
wcabus_cp <wesley@gotsharp.be>
2011-08-16 18:47:42 +0000

    - Fix: added the alpha channel component in the WeightedAverageInterpolator - Changed the ClickOnce settings: corrected the publisher and path in the start menu.

--- Pixelator/Pixelator/Properties/AssemblyInfo.cs
+++ Pixelator/Pixelator/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Pixelator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Wesley Cabus")]
 [assembly: AssemblyProduct("Pixelator")]
-[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyCopyright("Copyright © Wesley Cabus 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
